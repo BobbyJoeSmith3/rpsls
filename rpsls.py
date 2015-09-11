bff92dcbb9643191a6a678b51580299884314b53
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2015-09-11 17:54:23 -0500

    Define number_to_name function

--- rpsls.py
+++ rpsls.py
@@ -38,6 +38,19 @@ def name_to_number(name):
 def number_to_name(number):
     # convert number to a name using if/elif/else
     # don't forget to return the result!
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        return "What was that?! Please try again using '0, 1, 2, 3, or 4'"
+    
 
 
 def rpsls(player_choice):
