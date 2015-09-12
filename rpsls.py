228fcd272ccfee778754e972362e17f661039a3d
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2015-09-11 23:20:21 -0500

    Define rspls function

--- rpsls.py
+++ rpsls.py
@@ -50,26 +50,45 @@ def number_to_name(number):
         return "scissors"
     else:
         return "What was that?! Please try again using '0, 1, 2, 3, or 4'"
-    
+
 
 
 def rpsls(player_choice):
     # print a blank line to separate consecutive games
-
-    # print out the message for the player's choice
+    print ""
 
     # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+
+    # print out the message for the player's choice
+    print "Player chooses", player_choice
 
     # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0, 5)
 
     # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
 
     # print out the message for computer's choice
+    print "Computer chooses", comp_choice
 
     # compute difference of comp_number and player_number modulo five
+    x = (comp_number - player_number) % 5
 
     # use if/elif/else to determine winner, print winner message
-
+    if x == 1:
+        print "Computer wins!"
+    elif x == 2:
+        print "Computer wins!"
+    elif x == 3:
+        print "Player wins!"
+    elif x == 4:
+        print "Player wins!"
+    elif x == 0:
+        print "Player and computer tie"
+    else:
+        print "Error rpsls function"
+        
 
 # test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
 rpsls("rock")
